f098f2d70898f7d544e3a2685f3a18efd4e996f0
root <root@localhost.localdomain>
2021-03-30 23:17:50 +0800

    remove global style.css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {GlobalStyled} from './style.js';
+// import {GlobalStyled} from './style.js';
 
 ReactDOM.render(
   <Fragment>
-  	<GlobalStyled />
+
     <App />
   </Fragment>,
   document.getElementById('root')
